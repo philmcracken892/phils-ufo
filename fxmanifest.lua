fe2a107da7990b2e8bbb4b16b6078af74b726a34
philmcracken892 <MARC.KELLETT@GOOGLEMAIL.COM>
2025-06-08 12:21:34 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game "rdr3"
 rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
 lua54 'yes'
-author "Mack-phil Original By adnanberandai"
+author "Phil"
 
 shared_scripts {
     'config.lua',
@@ -19,6 +19,5 @@ server_scripts {
 
 dependencies {
     'rsg-core',
-    'rsg-target',
 	'ox_lib',
 }
